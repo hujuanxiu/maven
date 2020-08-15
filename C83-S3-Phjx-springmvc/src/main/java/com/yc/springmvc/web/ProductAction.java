dbf4387a748a283b8bba3d9f8d5fa23fdb890151
风吹来的小仙女 <@DESKTOP-S7B2RQ0>
2020-08-15 17:09:26 +0800

    请求参数和响应信息

--- C83-S3-Phjx-springmvc/src/main/java/com/yc/springmvc/web/ProductAction.java
+++ C83-S3-Phjx-springmvc/src/main/java/com/yc/springmvc/web/ProductAction.java
@@ -1,8 +1,13 @@
 package com.yc.springmvc.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yc.damai.po.DmCart;
+import com.yc.damai.po.DmUser;
 
 @RestController
 public class ProductAction {
@@ -35,15 +40,58 @@ public class ProductAction {
 		return "mod";
 	}
 	
-	@RequestMapping(path = "product.do" ,params = "op=select")
+	@GetMapping(path = "product.do" ,params = "op=select")
 	public String select() {
 		return "select";
 	}
 	
 	
-	@RequestMapping(path = "product.do" ,params = "op=mgr",headers = {"Host=localhost","Cookie"})
+	@GetMapping(path = "product.do" ,params = "op=mgr",headers = {"Host=localhost","Cookie"})
 	public String mgr() {
 		return "mgr";
 	}
 	
+	////////////////////请求参数接收/////////////////////////////
+	/**
+	 * SpringMVC对于简单参数：数字，字符串，布尔值可以直接将请求参数注入到方法参数中
+	 * 必须满足：1方法参数名=请求参数名
+	 *        2类型要匹配
+	 * @param user
+	 * @param pwd
+	 * @return
+	 */
+	@GetMapping("login.do")
+	public String login(String user,String pwd) {
+		return user+":"+pwd;
+	}
+	
+	/**
+	 * 如果对象接收请求参数，对象的属性名于请求参数名称映射
+	 * 请求参数--》对象  装箱--》Java 包装类 装箱
+	 * 对于一些特殊类型对象（日期），需要用springmvc转换器进行值的转换
+	 * @param user
+	 * @return
+	 */
+	@GetMapping("reg.do")
+	public String login(DmUser user,DmCart dc) {
+		return user.toString()+"<br>"+dc;
+	}
+	
+	
+	@GetMapping("pay.do")
+	public String pay(@RequestParam(name = "userid",//请求参数名
+	                                 defaultValue = "1",//默认值
+	                                 required = true)//必须提供
+	             String uid,Double money) {
+		return uid+":"+money;
+	}
+	
+	@GetMapping("finduser.do")
+	public DmUser findUser() {
+		DmUser du=new DmUser();
+		du.setId(100);
+		du.setCname("lisi");
+		du.setPassword("123");
+		return du;
+	}
 }
