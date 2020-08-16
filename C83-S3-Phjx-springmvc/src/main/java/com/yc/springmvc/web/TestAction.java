70bbab317e2c2652ea28918eb47adaa16a28e28b
风吹来的小仙女 <@DESKTOP-S7B2RQ0>
2020-08-16 15:59:00 +0800

    高级参数的注入

--- C83-S3-Phjx-springmvc/src/main/java/com/yc/springmvc/web/TestAction.java
+++ C83-S3-Phjx-springmvc/src/main/java/com/yc/springmvc/web/TestAction.java
@@ -1,5 +1,15 @@
 package com.yc.springmvc.web;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +36,35 @@ public class TestAction {
 		return "select";
 	}
 	
+	@RequestMapping("**/head")
+	public String head(@RequestHeader() String accept,
+			@RequestHeader(value = "Connection",required = true)String conn) {
+		return accept+"<br>"+conn;
+	}
+	
+	//获取cookie的值，要先在浏览器设置cookie
+	//document.cookie
+	@RequestMapping("**/cookie")
+	public String cookie(@CookieValue String user,
+			             @CookieValue String age,
+			             @RequestHeader String cookie) {
+		return user+"<br>"+age+"<br>"+cookie;
+	}
+	
+
+	/**
+	 * 请求对象，响应，会话
+	 * 添加servlet依赖
+	 * 应用上下文对象不能注入
+	 * 
+	 */
+	@RequestMapping("**/servlet")
+	public String servlet(HttpServletRequest request,
+			HttpServletResponse response,
+			HttpSession session,
+			InputStream in,
+			OutputStream out) {
+		return ""+request+"<br>"+response+"<br>"+session+"<br>"+in+"<br>"+out;
+	}
 	
 }
