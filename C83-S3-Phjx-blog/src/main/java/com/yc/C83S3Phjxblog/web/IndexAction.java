a0779a0458b4f0675631ae7f69930dbfcd1404c0
风吹来的小仙女 <@DESKTOP-S7B2RQ0>
2020-08-25 10:48:06 +0800

    文章详情

--- C83-S3-Phjx-blog/src/main/java/com/yc/C83S3Phjxblog/web/IndexAction.java
+++ C83-S3-Phjx-blog/src/main/java/com/yc/C83S3Phjxblog/web/IndexAction.java
@@ -5,7 +5,9 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.github.pagehelper.PageHelper;
 import com.yc.C83S3Phjxblog.dao.ArticleMapper;
 
 @Controller
@@ -16,10 +18,17 @@ public class IndexAction {
 	
 	
 	@GetMapping("/")
-	public String index(Model  model) {
-		
+	public String index(Model  model,@RequestParam(defaultValue = "1")int page) {
+		//在执行查询前，设置分页参数
+		//注意：必须是在查询方法执行前调用分页参数设置
+		PageHelper.startPage(page, 5);
 		model.addAttribute("alist", am.selectByNew());
 		return "index";
+	}
+	@GetMapping("article")
+	public String article(int id,Model model) {
+		model.addAttribute("article", am.selectById(id));
+		return "article";
 	}
 
 }
